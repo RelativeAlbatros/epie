fd63a007c59ca1293292d066b389223b0b2d005a
ZigZag35 <rayenpilote0@gmail.com>
2023-06-19 15:45:17 +0100

    save-file

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h> 
 #include <stdio.h>
 #include <stdarg.h> 
 #include <stdlib.h>
@@ -256,13 +257,14 @@ void editorInsertChar(int c) {
 
 char *editorRowsToString(int *buflen) {
     int totlen = 0;
-    for (int j = 0; j < E.numrows; j++)
+    int j;
+    for (j = 0; j < E.numrows; j++)
         totlen += E.row[j].size + 1;
     *buflen = totlen;
 
     char *buf = malloc(totlen);
     char *p = buf;
-    for (j = 0, j < E.numrows; j++) {
+    for (j = 0; j < E.numrows; j++) {
         memcpy(p, E.row[j].chars, E.row[j].size);
         p += E.row[j].size;
         *p = '\n';
@@ -290,6 +292,19 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+}
+
+int editorSave() {
+    if (E.filename == NULL) return -1;
+
+    int len;
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    ftruncate(fd, len);
+    write(fd, buf, len);
+    close(fd);
+    free(buf);
 }
 
 //}}}
