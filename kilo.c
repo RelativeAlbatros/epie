002ca1db698f9cbe5d986e56ec456d5363c0c34e
ZigZag35 <rayenpilote0@gmail.com>
2023-06-19 14:06:21 +0100

    make room for statusline

--- kilo.c
+++ kilo.c
@@ -317,9 +317,7 @@ void editorDrawRows(struct abuf *ab) {
         }
 
         abAppend(ab, "\x1b[K", 3);
-        if (y < E.screenrows - 1) {
-            abAppend(ab, "\r\n", 2);
-        }
+        abAppend(ab, "\r\n", 2);
     }
 
 }
@@ -447,6 +445,7 @@ void initEditor() {
     E.row = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+    E.screenrows -= 1;
 }
 
 //}}}
