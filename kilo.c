c9bc758f1860bd7ae18e2f5dcce498a39d8cc793
ZigZag35 <rayenpilote0@gmail.com>
2023-06-19 14:56:52 +0100

    message-bar-make-room

--- kilo.c
+++ kilo.c
@@ -349,6 +349,7 @@ void editorDrawStatusBar(struct abuf *ab) {
         }
     }
     abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\r\n", 2);
 }
 
 void editorRefreshScreen() {
@@ -486,7 +487,7 @@ void initEditor() {
     E.statusmsg_time = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-    E.screenrows -= 1;
+    E.screenrows -= 2;
 }
 
 //}}}
