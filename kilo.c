2b735b9c373e1b3f2090e3e5da3221bb0b69b821
ZigZag35 <rayenpilote0@gmail.com>
2023-06-19 16:37:11 +0100

    refactor editorSave()

--- kilo.c
+++ kilo.c
@@ -328,15 +328,22 @@ int editorSave() {
     char *buf = editorRowsToString(&len);
 
     int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-    if (fd == -1) return -1;
-    if (ftruncate(fd, len) == -1) return -1;
-    if (write(fd, buf, len) == -1) return -1;
+    if (fd != -1) {
+        if (ftruncate(fd, len) != -1) {
+            if (write(fd, buf, len) == len) {
+                close(fd);
+                free(buf);
+                E.dirty = 0;
+                editorSetStatusMessage("\"%s\" %dL, %dB Written", E.filename, E.screencols, len);
+                return 0;
+            }
+        }
+        close(fd);
+    }
 
-    close(fd);
     free(buf);
-    E.dirty = 0;
-
-    return 0;
+    editorSetStatusMessage("failed to save %s: %s", E.filename, strerror(errno));
+    return -1;
 }
 
 //}}}
@@ -545,11 +552,7 @@ void editorProcessKeypress() {
             break;
 
         case CTRL_KEY('s'):
-            if (!editorSave()) {
-                editorSetStatusMessage("\"%s\", %dLoC Written", E.filename, E.screencols);
-            } else {
-                editorSetStatusMessage("failed to save %s.", E.filename);
-            }
+            editorSave();
             break;
 
         case BACKSPACE:
