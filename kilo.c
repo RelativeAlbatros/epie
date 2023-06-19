f3f9fe4f4d6b8cf7ee99c255911be656c7181665
ZigZag35 <rayenpilote0@gmail.com>
2023-06-19 17:03:16 +0100

    del-char-row

--- kilo.c
+++ kilo.c
@@ -321,6 +321,11 @@ void editorDelChar() {
     if (E.cx > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
