d33e08935fc95b8f8b34f96352a2507cfc2faebf
ZigZag35 <rayenpilote0@gmail.com>
2023-06-21 14:11:44 +0100

    singleline-comment-start

--- kilo.c
+++ kilo.c
@@ -58,6 +58,7 @@ enum editorHighlight {
 struct editorSyntax {
 	char *filetype;
 	char **filematch;
+	char *singleline_comment_start;
 	int flags;
 };
 
@@ -98,6 +99,7 @@ struct editorSyntax HLDB[] = {
 	{
 		"c",
 		C_HL_extensions,
+		"//",
 		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
