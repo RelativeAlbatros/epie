c12833831136d53e5ab16a89e513c1814ab98819
ZigZag35 <rayenpilote0@gmail.com>
2023-06-21 11:56:09 +0100

    syntax-strings

--- kilo.c
+++ kilo.c
@@ -49,7 +49,7 @@ enum editorHighlight {
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
-#define HL_HIGHLIGHT_NUMBERS (1<<1)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 //}}}
 // data {{{
@@ -97,7 +97,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 
@@ -285,11 +285,29 @@ void editorUpdateSyntax(erow *row) {
     if (E.syntax == NULL) return;
 
     int prev_sep = 1;
+    int in_string = 0;
 
     int i = 0;
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i>0) ? row->hl[i-1] : HL_NORMAL;
+
+        if (E.syntax->flags && HL_HIGHLIGHT_STRINGS) {
+            if (in_string) {
+                row->hl[i] = HL_STRING;
+                if (c == in_string) in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            } else {
+                if (c == '\"' || c == '\'') {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }
 
         if (E.syntax->flags && HL_HIGHLIGHT_NUMBERS) {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
