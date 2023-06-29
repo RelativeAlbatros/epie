8be456e38728019b708c6b97c85786f0c5cc5e16
Rayen Daadaa <rayendaadaa35@gmail.com>
2023-06-29 17:14:57 +0100

    erow-c

--- src/epie/erow.h
+++ src/epie/erow.h
@@ -11,4 +11,14 @@ typedef struct erow {
 	int hl_open_comment;
 } erow;
 
+int editorRowCxToRx(erow *row, int cx);
+int editorRowRxToCx(erow *row, int rx);
+void editorUpdateRow(erow *row);
+void editorInsertRow(int at, char *s, size_t len);
+void editorFreeRow(erow *row);
+void editorDelRow(int at);
+void editorRowInsertChar(erow *row, int at, int c);
+void editorRowAppendString(erow *row, char *s, size_t len);
+void editorRowDelChar(erow *row, int at);
+
 #endif
